c9d10ba35a9a6045ae3920dfa2eff28cb0097187
Sarthak <sarthakstha24@gmail.com>
2023-11-15 22:48:35 +0545

    Necessary Changes.

--- resources/views/entry.blade.php
+++ resources/views/entry.blade.php
@@ -1,22 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('entrylayout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog Post Page</title>
-    <link rel="stylesheet" href="css/blog.css">
-</head>
-
-<body>
+@section('entry')
     <article>
         <h1>{{ $entry->title }}</h1>
         <div>
-            {{ $entry->body }}
+            <p>
+                {{ $entry->body }}
+            </p>
         </div>
-
-
+        <h3><a href="/">Return back</a></h3>
     </article>
-</body>
-
-</html>
+@endsection
