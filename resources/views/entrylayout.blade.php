1b8cccbd7272659f4ab45990d00995267af3d5eb
Sarthak <sarthakstha24@gmail.com>
2023-11-15 22:48:55 +0545

    Missing Files.

---
+++ resources/views/entrylayout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Entries of Zeus</title>
+    <link rel="stylesheet" href="/entry.css">
+</head>
+<body>
+<header>
+    @yield('banner')
+</header>
+@yield('entry')
+</body>
